87b1c7e990a83bd69213157670c5a1a06bc2378b
Jean-Denis Vauguet <jd@vauguet.fr>
2018-02-16 23:47:17 +0100

    Add a footer to the static UI with links to source code and Wikipedia

--- src/StaticUI.js
+++ src/StaticUI.js
@@ -10,9 +10,27 @@ export default (WrappedComponent) => {
     render() {
       return (
         <div className="fxp">
-          <h1 className="fxp-title">{APP_TITLE}</h1>
-          <h2 className="fxp-motto">{APP_MOTTO}</h2>
+          <header>
+            <h1 className="fxp-title">{APP_TITLE}</h1>
+            <h2 className="fxp-motto">{APP_MOTTO}</h2>
+          </header>
           <WrappedComponent {...this.props} />
+          <footer>
+            <ul className='fxp-u-list  fxp-footer-links'>
+              <li className='fxp-u-list__item'>
+                <a
+                  className='fxp-footer-link'
+                  href='https://github.com/chikamichi/fxpocket/'
+                >source</a>
+              </li>
+              <li className='fxp-u-list__item'>
+                <a
+                  className='fxp-footer-link'
+                  href='https://en.wikipedia.org/wiki/Exchange_rate'
+                >about exchange rates</a>
+              </li>
+            </ul>
+          </footer>
         </div>
       )
     }
